2ab8d0193f67ec544320c9fdf80fa1beef58df4a
krishna <krishnasharma111104@gmail.com>
2024-11-23 14:24:24 +0530

    router changed to hash router

--- colorwise/src/App.jsx
+++ colorwise/src/App.jsx
@@ -1,22 +1,23 @@
-import './App.css'
+import './App.css';
 import Frontpage from './components/Frontpage.jsx';
-import { BrowserRouter, Routes, Route } from'react-router-dom';
 import Photo from './components/Photo.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <>
-    <Navbar/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Frontpage />} />
-        <Route path="/photo-choose" element={<Photo />} />
-      </Routes>
-    </BrowserRouter>
-    <Footer/>
+      <Navbar />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Frontpage />} />
+          <Route path="/photo-choose" element={<Photo />} />
+        </Routes>
+      </HashRouter>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
